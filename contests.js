084f50545cc99f5190af0d696486e665c3a452e9
Raghav Agarwal <raghav.ag13@gmail.com>
2022-07-03 02:35:37 +0530

    remove deprecated libraries and actors

--- contests.js
+++ contests.js
@@ -1,4 +1,4 @@
-// web 
+// web
 const Soup = imports.gi.Soup;
 
 // file system
@@ -7,23 +7,22 @@ const GLib = imports.gi.GLib;
 
 // set timeout
 const Mainloop = imports.mainloop;
-const Lang = imports.lang;
+const GObject = imports.gi.GObject;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Self = ExtensionUtils.getCurrentExtension();
 
-const CODEFORCES_API_URL = "https://codeforces.com/api/contest.list?gym=false"
+const CODEFORCES_API_URL = "https://codeforces.com/api/contest.list?gym=false";
 
-var Contests = class {
-
-    constructor() {
-
+var Contests = class Contests {
+    constructor(updateCallback) {
         // https://github.com/ifl0w/RandomWallpaperGnome3/blob/develop/randomwallpaper%40iflow.space/wallpaperController.js
-        let xdg_cache_home = GLib.getenv('XDG_CACHE_HOME')
-        if (!xdg_cache_home) xdg_cache_home = `${GLib.getenv('HOME')}/.cache`
-        this.cacheLocation = `${xdg_cache_home}/${Self.metadata['uuid']}/`;
+        let xdg_cache_home = GLib.getenv("XDG_CACHE_HOME");
+        if (!xdg_cache_home) xdg_cache_home = `${GLib.getenv("HOME")}/.cache`;
+        this.cacheLocation = `${xdg_cache_home}/${Self.metadata["uuid"]}/`;
         this.cacheFile = this.cacheLocation + "contest.json";
 
+        this.updateCallback = updateCallback;
         this.retriesLeft = 5;
         this.retryTime = 1;
         this.refreshTimeout = null;
@@ -33,21 +32,30 @@ var Contests = class {
         this.refresh();
     }
 
+    stop() {
+        GLib.source_remove(this.refreshTimeout);
+    }
+
     loadFromFile() {
         this.allContests = [];
         try {
             let originalData = GLib.file_get_contents(this.cacheFile);
             if (originalData[0])
-                this.updateContests(JSON.parse(originalData[1]));
+                this.updateContests(JSON.parse(
+                    originalData[1] instanceof Uint8Array ?
+                        imports.byteArray.toString(originalData[1]) :
+                        originalData[1].toString()
+                ));
 
             this.setNextContest();
         } catch (e) {
-            global.log("ContestCountdown: No cache File / Cant open cache")
+            global.log("ContestCountdown: No cache File / Cant open cache");
+            global.log(e);
         }
     }
 
     saveToFile() {
-        GLib.mkdir_with_parents(this.cacheLocation, parseInt('0755', 8));
+        GLib.mkdir_with_parents(this.cacheLocation, parseInt("0755", 8));
         let file = Gio.file_new_for_path(this.cacheFile);
         let fstream = file.replace(null, false, Gio.FileCreateFlags.NONE, null);
         fstream.write(JSON.stringify(this.allContests), null);
@@ -57,42 +65,40 @@ var Contests = class {
     refresh() {
         this.retriesLeft--;
 
-        // remove refreshTimeout used when refresh fails
-        if (this.refreshTimeout) {
-            Mainloop.source_remove(this.refreshTimeout);
-            this.refreshTimeout = null;
-        }
-
         let session = new Soup.SessionAsync();
-        let message = Soup.Message.new('GET', CODEFORCES_API_URL);
+        let message = Soup.Message.new("GET", CODEFORCES_API_URL);
 
         session.queue_message(message, (session, message) => {
             try {
                 let response = JSON.parse(message.response_body.data);
-                if (response.status != "OK")
-                    throw "Got non OK status";
+                if (response.status != "OK") throw "Got non OK status";
 
                 this.updateContests(response.result);
+                this.updateCallback();
 
                 // if successful after retries, restore these
                 this.retriesLeft = 5;
                 this.retryTime = 1;
-                this.refreshTimeout = Mainloop.timeout_add_seconds(
+                this.refreshTimeout = GLib.timeout_add_seconds(
+                    GLib.PRIORITY_DEFAULT,
                     6 * 3600,
-                    Lang.bind(this, this.refresh)
+                    this.refresh
                 );
-
             } catch (e) {
-                global.log("ContestCountdown: Contest refresh failed\n retry left " + this
-                    .retriesLeft +
-                    "\n" + e);
+                global.log(
+                    "ContestCountdown: Contest refresh failed\n retry left " +
+                    this.retriesLeft +
+                    "\n" +
+                    e
+                );
 
                 if (this.retriesLeft) {
                     // if retries are left, then retry with exponentialy increasing time
                     this.retryTime *= 2;
-                    this.refreshTimeout = Mainloop.timeout_add_seconds(
+                    this.refreshTimeout = GLib.timeout_add_seconds(
+                        GLib.PRIORITY_DEFAULT,
                         this.retryTime,
-                        Lang.bind(this, this.refresh)
+                        this.refresh
                     );
                 } else {
                     // permanent fail, no more try
@@ -100,42 +106,48 @@ var Contests = class {
                     this.retryTime = 1;
                 }
             }
-        })
-    };
+        });
+        return false;
+    }
 
     updateContests(newContests) {
-
         newContests = this._filterContest(newContests);
 
-        newContests.forEach((contest) => {
-            if (!this.allContests.some((existingContest) => existingContest.id == contest
-                .id)) {
-                if (!('participating' in contest))
-                    contest.participating = true;
-                this.allContests.push(contest);
+        for (let contest of newContests) {
+            if ("participating" in contest) continue;
+            contest.participating = true;
+            for (let existingContest of this.allContests) {
+                if (existingContest.id == contest.id) {
+                    contest.participating = existingContest.participating;
+                }
             }
-        });
+        }
 
-        this.allContests = this._filterContest(this.allContests);
-
+        this.allContests = newContests;
 
         this.setNextContest();
         this.saveToFile();
     }
 
     _filterContest(contests) {
-        contests = contests.filter((contest) => contest.startTimeSeconds && contest.phase == "BEFORE" && this
-            .secondsTillContest(contest) >= 0);
+        contests = contests.filter(
+            (contest) =>
+                contest.startTimeSeconds &&
+                contest.phase == "BEFORE" &&
+                this.secondsTillContest(contest) >= 0
+        );
 
         contests.sort((a, b) => {
-            return parseInt(a.startTimeSeconds) - parseInt(b.startTimeSeconds)
+            return parseInt(a.startTimeSeconds) - parseInt(b.startTimeSeconds);
         });
 
         return contests;
     }
 
     secondsTillContest(contest) {
-        return Math.floor((new Date(contest.startTimeSeconds * 1000) - new Date()) / 1000);
+        return Math.floor(
+            (new Date(contest.startTimeSeconds * 1000) - new Date()) / 1000
+        );
     }
 
     setNextContest() {
@@ -151,8 +163,7 @@ var Contests = class {
     secondsTillNextContest() {
         if (this.nextContest) {
             let timeDiff = this.secondsTillContest(this.nextContest);
-            if (timeDiff >= 0)
-                return timeDiff;
+            if (timeDiff >= 0) return timeDiff;
             else {
                 this.setNextContest();
                 return this.secondsTillNextContest();
@@ -163,12 +174,9 @@ var Contests = class {
             // if failed to load, return -Infinity
             // if no upcoming contest, return Infinity
 
-            if (this.retriesLeft < 5)
-                return -1;
-            if (this.allContests.length == 0)
-                return -Infinity;
+            if (this.retriesLeft < 5) return -1;
+            if (this.allContests.length == 0) return -Infinity;
             return Infinity;
         }
-
-    };
-}
+    }
+};
--- popupmenu.js
+++ popupmenu.js
@@ -17,196 +17,193 @@ const Self = imports.misc.extensionUtils.getCurrentExtension();
 // make nextcontest global to access in onClick of contestElement
 var nextContestElement;
 
-var contestElement = GObject.registerClass({
-    GTypeName: 'contestElement ',
-}, class contestElement extends PopupMenu.PopupBaseMenuItem {
+var contestElement = GObject.registerClass(
+    {},
+    class contestElement extends PopupMenu.PopupBaseMenuItem {
+        _init(contest, contests) {
+            super._init();
+            // save these to access in onClick
+            this.contest = contest;
+            this.contests = contests;
 
-    _init(contest, contests) {
-        super._init();
-        // save these to access in onClick
-        this.contest = contest;
-        this.contests = contests;
+            // create container to contain checkbox and contest details
+            this._container = new St.BoxLayout({
+                vertical: false,
+            });
 
-        // create container to contain checkbox and contest details
-        this._container = new St.BoxLayout({
-            vertical: false,
-        });
+            // create checkbox
+            this._checkbox = new CheckBox.CheckBox();
+            this._checkbox.checked = contest.participating;
+            this._checkbox.connect("clicked", () => this.onClick());
 
-        // create checkbox
-        this._checkbox = new CheckBox.CheckBox();
-        this._checkbox.checked = contest.participating;
-        this._checkbox.connect('clicked', () => this.onClick());
+            // make contest label
+            this._contestLabel = new ContestDetails(contest);
 
-        // make contest label
-        this._contestLabel = new ContestDetails(contest);
+            // add both to container
+            this._container.add_child(this._checkbox);
+            this._container.add_child(this._contestLabel);
 
-        // add both to container
-        this._container.add_child(this._checkbox);
-        this._container.add_child(this._contestLabel);
+            // add container to menu item
+            this.add_child(this._container);
 
-        // add container to menu item
-        this.actor.add_child(this._container);
-
-        // open registration page on click
-        this.actor.connect('button-press-event', function() {
-            Util.spawn(['xdg-open', "https://codeforces.com/contestRegistration/" + contest.id]);
-        });
+            // open registration page on click
+            this.connect("button-press-event", function() {
+                Util.spawn([
+                    "xdg-open",
+                    "https://codeforces.com/contestRegistration/" + contest.id,
+                ]);
+            });
+        }
+        onClick() {
+            this.contest.participating = this._checkbox.checked;
+            this.contests.setNextContest();
+            nextContestElement.update();
+            this.contests.saveToFile();
+        }
     }
-    onClick() {
-        this.contest.participating = this._checkbox.checked;
-        this.contests.setNextContest();
-        nextContestElement.update();
-        this.contests.saveToFile();
+);
 
-    }
-});
+var ContestDetails = GObject.registerClass(
+    {},
+    class ContestDetails extends St.BoxLayout {
+        _init(contest) {
+            super._init({
+                vertical: true,
+            });
 
-var ContestDetails = GObject.registerClass({
-    GTypeName: 'ContestDetails',
-}, class ContestDetails extends St.BoxLayout {
+            var nameLabel = new St.Label({
+                text: contest.name,
+                style_class: "cc-contest-name",
+            });
+            nameLabel.clutter_text.line_wrap = true;
 
-    _init(contest) {
-        super._init({
-            vertical: true,
-        });
+            let hh = Math.floor(contest.durationSeconds / 3600);
+            let mm = Math.floor((contest.durationSeconds % 3600) / 60);
 
-        var nameLabel = new St.Label({
-            text: contest.name,
-            style_class: "cc-contest-name",
-        });
-        nameLabel.clutter_text.line_wrap = true;
+            var details =
+                `Date\t\t:  ${new Date(1000 * contest.startTimeSeconds).toLocaleFormat(
+                    "%A %d %B %Y"
+                )} ` +
+                `\nTime\t\t:  ${new Date(
+                    1000 * contest.startTimeSeconds
+                ).toLocaleFormat("%r")} ` +
+                `\nDuration\t:  ${hh} hours ${mm} minutes`;
 
-        let hh = Math.floor(contest.durationSeconds / 3600);
-        let mm = Math.floor((contest.durationSeconds % 3600) / 60);
-
-        var details = `Date\t\t:  ${new Date(1000 * contest.startTimeSeconds).toLocaleFormat("%A %d %B %Y")} ` +
-            `\nTime\t\t:  ${new Date(1000 * contest.startTimeSeconds).toLocaleFormat("%r")} ` +
-            `\nDuration\t:  ${hh} hours ${mm} minutes`;
-
-        var detailsLabel = new St.Label({
-            text: details,
-            style_class: "cc-inline",
-        });
-
-
-        this.add_child(nameLabel);
-        this.add_child(detailsLabel);
-    }
-
-});
-
-var NextContestElement = GObject.registerClass({
-    GTypeName: 'NextContestElement',
-}, class NextContestElement extends PopupMenu.PopupBaseMenuItem {
-    _init(contests) {
-        super._init();
-        // make container
-        this.contests = contests;
-        this.contest = contests.nextContest;
-        this._container = new St.BoxLayout({
-            vertical: true
-        });
-
-        // make heading
-        this._headingLabel = new St.Label({
-            text: 'Next Contest',
-            style_class: "cc-contest-heading",
-        });
-
-        if (this.contest)
-            this._contestLabel = new ContestDetails(this.contest);
-        else
-            this._contestLabel = new St.Label({
-                text: 'No Upcoming Contest',
+            var detailsLabel = new St.Label({
+                text: details,
                 style_class: "cc-inline",
             });
 
-        // add two labels to container
-        this._container.add_child(this._headingLabel);
-        this._container.add_child(this._contestLabel);
-
-        this.actor.add_child(this._container);
-
-        // open codeforces on click
-        this.actor.connect('button-press-event', () => {
-            global.log("click");
-            Util.spawn(['xdg-open', "https://codeforces.com/contestRegistration/" + this.contests
-                .nextContest.id
-            ]);
-        });
+            this.add_child(nameLabel);
+            this.add_child(detailsLabel);
+        }
     }
-    update() {
-        if (this.contests.nextContest != this.contest) {
-            this.contest = this.contests.nextContest;
-            this._container.remove_child(this._contestLabel);
-            if (this.contest)
-                this._contestLabel = new ContestDetails(this.contest);
+);
+
+var NextContestElement = GObject.registerClass(
+    {},
+    class NextContestElement extends PopupMenu.PopupBaseMenuItem {
+        constructor(contests) {
+            super();
+            // make container
+            this.contests = contests;
+            this.contest = contests.nextContest;
+            this._container = new St.BoxLayout({
+                vertical: true,
+            });
+
+            // make heading
+            this._headingLabel = new St.Label({
+                text: "Next Contest",
+                style_class: "cc-contest-heading",
+            });
+
+            if (this.contest) this._contestLabel = new ContestDetails(this.contest);
             else
                 this._contestLabel = new St.Label({
-                    text: 'No Upcoming Contest',
+                    text: "No Upcoming Contest",
                     style_class: "cc-inline",
                 });
 
+            // add two labels to container
+            this._container.add_child(this._headingLabel);
             this._container.add_child(this._contestLabel);
+
+            this.add_child(this._container);
+
+            // open codeforces on click
+            this.connect("button-press-event", () => {
+                global.log("click");
+                Util.spawn([
+                    "xdg-open",
+                    "https://codeforces.com/contestRegistration/" +
+                    this.contests.nextContest.id,
+                ]);
+            });
         }
+        update() {
+            if (this.contests.nextContest != this.contest) {
+                this.contest = this.contests.nextContest;
+                this._container.remove_child(this._contestLabel);
+                if (this.contest) this._contestLabel = new ContestDetails(this.contest);
+                else
+                    this._contestLabel = new St.Label({
+                        text: "No Upcoming Contest",
+                        style_class: "cc-inline",
+                    });
 
+                this._container.add_child(this._contestLabel);
+            }
+        }
     }
-});
-
+);
 
 class AllContestsList extends PopupMenu.PopupMenuSection {
-
     constructor(contests) {
         super();
 
-        this.actor = new St.ScrollView({
+        let scrollview = new St.ScrollView({
             hscrollbar_policy: Gtk.PolicyType.NEVER,
-            vscrollbar_policy: Gtk.PolicyType.AUTOMATIC
+            vscrollbar_policy: Gtk.PolicyType.AUTOMATIC,
         });
+        this.innerMenu = new PopupMenu.PopupMenuSection();
+        scrollview.add_actor(this.innerMenu.actor);
 
-        this.actor.add_actor(this.box);
+        // this.actor.add_actor(this.box);
+        this.actor.add_actor(scrollview);
 
         for (let contest of contests.allContests) {
-            this.addMenuItem(new contestElement(contest, contests));
+            this.innerMenu.addMenuItem(new contestElement(contest, contests));
         }
-
     }
+}
 
-};
-
-var AllContestHeading =
-    GObject.registerClass({
-        GTypeName: 'AllContestHeading ',
-    }, class AllContestHeading extends PopupMenu.PopupBaseMenuItem {
-
+let AllContestHeading = GObject.registerClass(
+    {},
+    class AllContestHeading extends PopupMenu.PopupBaseMenuItem {
         _init(params) {
-
             super._init(params);
             this._headingLabel = new St.Label({
-                text: 'All Contest',
+                text: "All Contest",
                 style_class: "cc-contest-heading",
             });
-            this.actor.add_child(this._headingLabel);
-            this.actor.connect('button-press-event', function() {
-                Util.spawn(['xdg-open', "https://codeforces.com/contests"]);
+            this.add_child(this._headingLabel);
+            this.connect("button-press-event", function() {
+                Util.spawn(["xdg-open", "https://codeforces.com/contests"]);
             });
         }
-
-    });
-
-
+    }
+);
 
 function PopMenuMaker(parent) {
-
     // Next contest element
+    parent.menu.removeAll();
     nextContestElement = new NextContestElement(parent.contests);
     parent.menu.addMenuItem(nextContestElement);
-
     // seperator line
     parent.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-    // All contest 
+    // All contest
     parent.menu.addMenuItem(new AllContestHeading());
     parent.menu.addMenuItem(new AllContestsList(parent.contests));
-
 }
