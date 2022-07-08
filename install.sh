f5f756256a3fa2e56d9f24f31b54c31630cafab7
Raghav Agarwal <60528983+rag-hav@users.noreply.github.com>
2022-07-08 18:18:07 +0530

    make installer version aware

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 EXTENSION_DIR="$HOME/.local/share/gnome-shell/extensions/contestcountdown@raghav/"
+GNOME_VERSION="$( gnome-shell --version | cut -f '3' -d ' ' | cut -f '1' -d '.')" 
+
+echo "Installing in $EXTENSION_DIR"
+echo "Gnome version $GNOME_VERSION"
+echo 
 
 # remove previous installation
 if [ -d "$EXTENSION_DIR" ]; then
@@ -11,7 +16,14 @@ fi
 
 # clone from the repository
 echo "Cloning from repository..."
-git clone https://github.com/rag-hav/ContestCountdown --branch master --single-branch "$EXTENSION_DIR" || (printf "\nCouldn't clone repository" && exit 1)
+git clone https://github.com/rag-hav/ContestCountdown --branch master --single-branch "$EXTENSION_DIR" || (printf "\nCouldn't clone repository\n" && exit 1)
+
+
+if (( GNOME_VERSION < 40 )); then
+    cd "$EXTENSION_DIR" || ( printf "\nFailed to change directory" && exit 1 )
+    echo "Checking out old commit for Gnome version < 40"
+    git checkout 32d1b1533a1ef24dae98c64389c2058f1a50c2f3
+fi
 
 # enable the extension
 echo "Enabling extension..."
